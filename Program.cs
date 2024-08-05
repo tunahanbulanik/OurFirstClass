f78235bf58ed4b2cc0f787915d094d0ad96aa9b1
Tuna Han Bulanık <tunahanbulanik@outlook.com>
2024-08-05 19:03:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Channels;
+namespace OurFirstClass
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+
+        public Person(string ad, string soyad, DateTime dogumTarihi)
+        {
+            Name = ad;
+            LastName = soyad;
+            BirthDay = dogumTarihi;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {LastName}, Doğum Tarihi: {BirthDay.ToShortDateString()}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person studentOne = new Person("Tuna","Bulanık", new DateTime(1999 , 7 , 13));
+            Person studentTwo = new Person("Ahmet", "Ahmetoğlu", new DateTime(2003 , 9, 18));
+
+            Console.WriteLine("Öğrenciler:");
+            Console.WriteLine($"{studentOne} \n {studentTwo}");
+        }
+    }
+}
